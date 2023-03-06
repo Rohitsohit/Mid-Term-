0392f1feeb0b235c02038f2258e744631f058cbf
Rohitsohit <itsrohitemail@gmail.com>
2023-03-05 21:44:42 -0500

    Comment

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+  // Enrollment Number :- 301315172
+  // Name :- Rohit 
+
+module.exports = {
+  //local MongoDB deployment ->
+ 
+  "URI": "mongodb+srv://Rohit:123@cluster0.xqoa6nc.mongodb.net/?retryWrites=true&w=majority"
+};
---
+++ server/models/books.js
@@ -0,0 +1,19 @@
+  // Enrollment Number :- 301315172
+  // Name :- Rohit 
+
+
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
